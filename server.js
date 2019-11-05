2a311bde25994a189c65fb447a2e5cb7a83af239
Feng Lian <b878788242@gmail.com>
2019-11-04 22:56:40 -0500

    assignment3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,7 +11,18 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 //app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//connect to MongoDB in the cloud
+const cstring = "mongodb+srv://flian2:assignment3@cluster0-53fk4.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(cstring, {useNewUrlParser: true})
+.then(()=>{
+    console.log(`Database is connected`)
+})
+//The catch block will only be executed if the connection failed
+.catch(err=>{
+    console.log(`Something went wrong : ${err}`);
+})
 
 //routes
 app.get('/',(req, res) =>
@@ -22,6 +34,141 @@ app.get('/registration', (req, res) =>
 {
     res.render("registration");
 });
+
+app.get('/login', (req, res) =>
+{
+    res.render("login"); 
+});
+
+app.post('/login', (req,res) =>
+{
+    const errors = [];
+    if (req.body.username == "")
+    {
+        errors.push("Please enter your username!!");
+    }
+
+    if (req.body.password == "")
+    {
+        errors.push("Please enter your password!!");
+    }
+
+    if (errors.length > 0)
+    {
+        res.render("login",{login:errors})
+    }
+    //There are no errors
+    else
+    {
+        
+    }
+})
+
+app.post('/registration', (req, res) =>
+{
+    const errors = [];
+    //start with uppercase, and only alphabet allowed
+    const pattern1 = /^[A-Z][a-zA-Z]+/
+    //start with letter, at least 6 character
+    const pattern2 = /^[a-zA-Z].{5}/;
+    
+    if (req.body.email == "")
+    {
+        errors.push("Please enter your email!!");
+    }
+
+    if (req.body.first == "" || !pattern1.test(req.body.first))
+    {
+        errors.push("First name must start with uppercase, and only alphabet allowed!!");
+    }
+
+    if (req.body.last == "" || !pattern1.test(req.body.last))
+    {
+        errors.push("Last name must start with uppercase, and only alphabet allowed!!");
+    }
+
+    if (req.body.password == "" || !pattern2.test(req.body.password))
+    {
+        errors.push("Password must start with letter, at least 6 character!!");
+    }
+
+    if (req.body.bdate == "")
+    {
+        errors.push("Please enter your birthday!!");
+    }
+
+
+    if (errors.length > 0)
+    {
+        res.render("registration", {regis:errors})
+    }
+    else
+    {
+        //connect to MongoDB, create schema
+        const Schema = mongoose.Schema;
+        const accountSchema = new Schema({
+            email: String,
+            first: String,
+            last: String,
+            password: String,
+            date: String
+        });
+        
+        //create account model
+        const Accounts = mongoose.model('Accounts', accountSchema);
+
+        const formData ={
+            email: req.body.email,
+            first: req.body.first,
+            last: req.body.last,
+            password: req.body.password,
+            date: req.body.bdate,
+        }
+
+        //create account document
+        const ac = new Accounts(formData);
+        ac.save()
+        .then(() =>
+        {
+            console.log(`Account inserted to database`);
+        })
+        .catch((err)=>
+        {
+            console.log(`Account not inserted ${err}`);
+        })
+
+        //send Email
+        const nodemailer = require('nodemailer');
+        const sgTransport = require('nodemailer-sendgrid-transport');
+
+         const options = {
+            auth: 
+            {
+                api_key: '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'
+            }
+        }
+
+        const mailer = nodemailer.createTransport(sgTransport(options));
+
+        const email = {
+            to: `${req.body.email}`,
+            from: 'flian2@myseneca.ca',
+            subject: 'Welcome',
+            text: `Welcome to AirB&B`,
+            html: `Welcome to AirB&B`
+        };
+         
+        mailer.sendMail(email, (err, res)=> {
+            if (err) { 
+                console.log(err) 
+            }
+            console.log(res);
+        });
+
+         //REDIRECT THE USER TO THE DASHBOARD ROUTE
+           res.redirect("/room_listing");
+    }
+})
 
 app.get("/room_listing",(req,res)=>
 {
